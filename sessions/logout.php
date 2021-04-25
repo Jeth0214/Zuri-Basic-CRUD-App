ae71a9b1532acb876959c24b87421a5cba924802
Jeth0214 <rolandjethrosuyom@yahoo.com>
2021-04-25 19:05:55 +0300

    unset session id

--- sessions/logout.php
+++ sessions/logout.php
@@ -5,5 +5,6 @@ session_start();
 $_SESSION['status'] = "Invalid";
 
 unset($_SESSION['username']);
+unset($_SESSION['userid']);
 
 echo "<script>window.location.href = '../index.php'</script>";
--- sessions/session.php
+++ sessions/session.php
@@ -5,6 +5,7 @@ if ($_SESSION['status'] === "Invalid" || empty($_SESSION['status'])) {
     $_SESSION['status'] = "Invalid";
 
     unset($_SESSION['username']);
+    unset($_SESSION['userid']);
 
     echo "<script>window.location.href = '../../index.php'</script>";
 }
